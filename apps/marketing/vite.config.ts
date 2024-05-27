48f6eafbac78fe38399fd31089da92b4bbd1d6d9
web2integrators <jay@officeintegrator.com>
2024-05-26 19:59:04 -0700

    ic

---
+++ apps/marketing/vite.config.ts
@@ -0,0 +1,42 @@
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { qwikNxVite } from 'qwik-nx/plugins';
+
+export default defineConfig({
+  cacheDir: '../../node_modules/.vite/apps/marketing',
+  plugins: [
+    qwikNxVite(),
+    qwikCity(),
+    qwikVite({
+      client: {
+        outDir: '../../dist/apps/marketing/client',
+      },
+      ssr: {
+        outDir: '../../dist/apps/marketing/server',
+      },
+      tsconfigFileNames: ['tsconfig.app.json'],
+    }),
+    tsconfigPaths({ root: '../../' }),
+  ],
+  server: {
+    fs: {
+      // Allow serving files from the project root
+      allow: ['../../'],
+    },
+  },
+  preview: {
+    headers: {
+      'Cache-Control': 'public, max-age=600',
+    },
+  },
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+});
